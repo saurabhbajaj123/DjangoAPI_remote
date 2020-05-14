85ffb8a1d69f31f962b9f9cf5e088c93ef34c063
saurabh717 <saurabh.rb440@gmail.com>
2020-05-14 20:52:51 +0530

    adding form for user to input data; code to run saved ML model on the user's data

--- MyAPI/forms.py
+++ MyAPI/forms.py
@@ -1,8 +1,23 @@
 from django.forms import ModelForm
 from .models import Approvals
+from django import forms
 
 class MyForm(ModelForm):
     class Meta:
         model=Approvals
         fields='__all__'
-        
+
+class ApprovalForm(forms.Form):
+    firstname = forms.CharField(max_length=15)
+    lastname = forms.CharField(max_length=15)
+    Dependents = forms.IntegerField()
+    ApplicantIncome = forms.IntegerField()
+    CoapplicantIncome = forms.IntegerField()
+    LoanAmount = forms.IntegerField()
+    Loan_Amount_Term = forms.IntegerField()
+    Credit_History = forms.IntegerField()
+    Gender = forms.ChoiceField(choices=[('Male', 'Male'), ('Female', 'Female')])
+    Married = forms.ChoiceField(choices=[('Yes', 'Yes'), ('No', 'No')])
+    Education = forms.ChoiceField(choices=[('Graduate', 'Graduate'), ('Not_Graduate', 'Not_Graduate')])
+    Self_Employed = forms.ChoiceField(choices=[('Yes', 'Yes'), ('No', 'No')])
+    Property_Area = forms.ChoiceField(choices=[('Rural', 'Rural'), ('Semiurban', 'Semiurban'), ('Urban', 'Urban')])
--- MyAPI/views.py
+++ MyAPI/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
-from .forms import MyForm
+from .forms import MyForm, ApprovalForm
 from rest_framework import viewsets
 from rest_framework.decorators import api_view
 from django.core import serializers
 from rest_framework.response import Response
 from rest_framework import status
 from django.http import JsonResponse
+from django.contrib import messages
 from rest_framework.parsers import JSONParser
 from .models import Approvals
 from .serializers import approvalsSerializers
@@ -17,6 +18,7 @@ from sklearn import preprocessing
 import pandas as pd
 from django.http import HttpResponse
 import os
+import keras
 
 # def index(request):
 #     return HttpResponse("Hello, world. You're at the MyAPI index.")
@@ -34,22 +36,87 @@ def myform(request):
     else:
         form = MyForm()
 
-@api_view(["POST"])
-def approvereject(request):
+
+def ohevalue(df):
+    ohe_col=['Dependents', 'ApplicantIncome', 'CoapplicantIncome', 'LoanAmount',
+       'Loan_Amount_Term', 'Credit_History', 'Gender_Female', 'Gender_Male',
+       'Married_No', 'Married_Yes', 'Education_Graduate',
+       'Education_Not Graduate', 'Self_Employed_No', 'Self_Employed_Yes',
+       'Property_Area_Rural', 'Property_Area_Semiurban',
+       'Property_Area_Urban']
+    cat_columns=['Gender', 'Married', 'Education', 'Self_Employed', 'Property_Area']
+    df_processed=pd.get_dummies(df, columns=cat_columns)
+    new_dict={}
+    for i in ohe_col:
+        if i in df_processed.columns:
+            new_dict[i]=df_processed[i].values
+        else:
+            new_dict[i]=0
+    new_df=pd.DataFrame(new_dict)
+    return new_df
+
+
+# @api_view(["POST"])
+def approvereject(unit):
     try:
-        model_name = os.path.abspath(os.path.join(os.getcwd(), os.pardir)).replace('\\', '/') + '/MLmodel/loan_madel.pkl'
+        keras.backend.clear_session() # this is needed other wise we get an error - ValueError("Tensor %s is not an element of this graph." % obj)
+        model_name = os.path.abspath(os.getcwd()).replace('\\', '/') + '/MLmodel/loan_madel.pkl'
+        # print('start model loading')
         mdl = joblib.load(model_name)
-        mydata=request.data
-        unit=np.array(list(mydata.values()))
-        unit=unit.reshape(1,-1)
-        scaler_filename = os.path.abspath(os.path.join(os.getcwd(), os.pardir)).replace('\\', '/') + '/MLmodel/scaler.pkl'
-        scalers=joblib.load(scaler_filename)
+        # print('end model loading')
+
+        # # mydata=request.data
+        # # unit=np.array(list(mydata.values()))
+        # # unit=unit.reshape(1,-1)
+
+        scaler_filename = os.path.abspath(os.getcwd()).replace('\\', '/') + '/MLmodel/scaler.pkl'
+        # print('start scaler loading from {}'.format(scaler_filename))
+        scalers = joblib.load(scaler_filename)
+        # print('end scaler loading')
+        # print('start scaling')
         X=scalers.transform(unit)
+        # print('end scaling')
+        X=unit
+        # print('start predicting')
         y_pred=mdl.predict(X)
+        # print('end predicting')
+        # print('start filter')
         y_pred=(y_pred>0.58)
+        # print('end filter')
+        # print("y_pred = {}".format(y_pred))
         newdf=pd.DataFrame(y_pred, columns=['Status'])
         newdf=newdf.replace({True:'Approved', False:'Rejected'})
-        return JsonResponse('Your Status is {}'.format(newdf), safe=False)
+        # print(newdf)
+        # return JsonResponse('Your Status is {}'.format(newdf), safe=False)
+        return ('Your status is {}'.format(newdf.values[0][0]))
     except ValueError as e:
         return Response(e.args[0], status.HTTP_400_BAD_REQUEST)
 
+def custcontact(request):
+    if request.method=='POST':
+        form=ApprovalForm(request.POST)
+        if form.is_valid():
+            firstname=form.cleaned_data['firstname']
+            lastname=form.cleaned_data['lastname']
+            Dependents=form.cleaned_data['Dependents']
+            ApplicantIncome=form.cleaned_data['ApplicantIncome']
+            CoapplicantIncome=form.cleaned_data['CoapplicantIncome']
+            LoanAmount=form.cleaned_data['LoanAmount']
+            Loan_Amount_Term=form.cleaned_data['Loan_Amount_Term']
+            Credit_History=form.cleaned_data['Credit_History']
+            Gender=form.cleaned_data['Gender']
+            Married=form.cleaned_data['Married']
+            Education=form.cleaned_data['Education']
+            Self_Employed=form.cleaned_data['Self_Employed']
+            Property_Area=form.cleaned_data['Property_Area']
+            myDict=(request.POST).dict() # need to understand this part
+            df=pd.DataFrame(myDict, index=[0])
+            # print('x')
+            answer = (approvereject(ohevalue(df))) # this is just a function that takes df as input and gives result of approval/rejection
+            messages.success(request, 'Application status: {}'.format(answer))
+            # print('x')
+            print(firstname, lastname, Dependents, Married, Property_Area)
+    
+    form=ApprovalForm()
+
+    return render(request, 'myform/form.html', {'form':form})
