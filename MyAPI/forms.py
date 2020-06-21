903cc99fbe1627e2da30d3216131f241e00cdb90
saurabh717 <saurabh.rb440@gmail.com>
2020-06-21 16:21:18 +0530

    adding placeholders for empty input boxes in form

--- MyAPI/forms.py
+++ MyAPI/forms.py
@@ -8,14 +8,14 @@ class MyForm(ModelForm):
         fields='__all__'
 
 class ApprovalForm(forms.Form):
-    firstname = forms.CharField(max_length=15)
-    lastname = forms.CharField(max_length=15)
-    Dependents = forms.IntegerField()
-    ApplicantIncome = forms.IntegerField()
-    CoapplicantIncome = forms.IntegerField()
-    LoanAmount = forms.IntegerField()
-    Loan_Amount_Term = forms.IntegerField()
-    Credit_History = forms.IntegerField()
+    firstname = forms.CharField(max_length=15, widget=forms.TextInput(attrs={'placeholder': 'Enter firstname'}))
+    lastname = forms.CharField(max_length=15, widget=forms.TextInput(attrs={'placeholder': 'Enter lastname'}))
+    Dependents = forms.IntegerField(widget=forms.NumberInput(attrs={'placeholder': 'Enter number of dependents'}))
+    ApplicantIncome = forms.IntegerField(widget=forms.NumberInput(attrs={'placeholder': 'Enter mosthly gross income'}))
+    CoapplicantIncome = forms.IntegerField(widget=forms.NumberInput(attrs={'placeholder': 'Enter co-applicant monthly gross income'}))
+    LoanAmount = forms.IntegerField(widget=forms.NumberInput(attrs={'placeholder': 'Requested loan amount'}))
+    Loan_Amount_Term = forms.IntegerField(widget=forms.NumberInput(attrs={'placeholder': 'Loan term in months'}))
+    Credit_History = forms.ChoiceField(choices=[(1, '1'), (0, '0')])
     Gender = forms.ChoiceField(choices=[('Male', 'Male'), ('Female', 'Female')])
     Married = forms.ChoiceField(choices=[('Yes', 'Yes'), ('No', 'No')])
     Education = forms.ChoiceField(choices=[('Graduate', 'Graduate'), ('Not_Graduate', 'Not_Graduate')])
