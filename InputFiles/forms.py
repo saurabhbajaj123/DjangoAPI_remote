4717bf8e7f7ccea7e8a503fa0f4c83be7014441f
saurabh717 <saurabh.rb440@gmail.com>
2020-07-17 18:39:32 +0530

    Multiselect choice form

--- InputFiles/forms.py
+++ InputFiles/forms.py
@@ -1,5 +1,24 @@
 from django import forms
+from . import views
+from django.contrib.auth.forms import UserCreationForm
 
 class UploadFileForm(forms.Form):
     # title = forms.CharField(max_length=500)
     file = forms.FileField(required=False)
+
+class UserCreationForm(forms.Form):
+    columns = ['id', 'packages_required', 'pakcages_installed', 'exactly']
+
+    # def __init__(self, *args, **kwargs):
+    #     extra = kwargs.pop('extra')
+    #     super(UserCreationForm, self).__init__(*args, **kwargs)
+    #     self.columns = extra
+    
+    def getChoices(list_):    
+        list_column_tuples = []
+        for i in zip(list_, list_):
+            list_column_tuples.append(i)
+        OPTIONS = tuple(list_column_tuples)
+        return OPTIONS
+    Columns = forms.MultipleChoiceField(widget=forms.CheckboxSelectMultiple, 
+                                          choices=getChoices(columns))
--- InputFiles/views.py
+++ InputFiles/views.py
@@ -1,21 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
-from .forms import UploadFileForm
+from django.template import RequestContext
 from django.core.files.storage import FileSystemStorage
 from django.conf import settings
+from .forms import UploadFileForm, UserCreationForm
 import json
 import csv, io, codecs, re
 import pandas as pd
 # import papaparse as Papa
 # Create your views here.
-
+columns = []
 def upload_file(request):
     if request.method == 'POST':
         print('1')
         # uploaded_file = request.FILES['document']
         # print(uploaded_file.name)
         # print(uploaded_file.size)
-        form = UploadFileForm(request.POST, request.FILES) # did not understood the part
+        form = UploadFileForm(request.POST, request.FILES) # did not understood the 
         print('2')
         print(form.is_valid())
         if form.is_valid():
@@ -28,6 +29,8 @@ def upload_file(request):
             print('reading csv into pandas')
             df = pd.read_csv(uploaded_file)
             print(df.head())
+            columns = df.columns
+            print(columns)
 
             # creating a filesystem object to save file
             # defines the location where the files will be stored
@@ -36,11 +39,25 @@ def upload_file(request):
             # fs.save(uploaded_file.name, uploaded_file)
             print('2.9')
             return render(request, 'upload_forms/upload.html', {'form': form})
-        
     else:
         print('3')
         form = UploadFileForm()
     return render(request, 'upload_forms/upload.html', {'form': form})
     # return render(request, 'upload_forms/upload.html')
+
+
+def multiple_choices(request):
+    if request.method == 'POST':
+        columns = ['id', 'packages_required', 'pakcages_installed']
+        form2 = UserCreationForm(request.POST)
+        if form2.is_valid():
+            
+            columns = form2.cleaned_data #.get('columns')
+
+            print(columns) # {'Columns': ['id', 'packages_required', 'pakcages_installed']}
+    else:
+        form2 = UserCreationForm()
+
+    return render(request, 'upload_forms/upload.html', {'form': form2})
 
 
